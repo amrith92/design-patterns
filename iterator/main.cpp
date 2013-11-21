9621e0c23821ba92c53b54188dbb388d5b2fef61
Amrith Nayak <amrith92@gmail.com>
2013-11-21 18:15:34 +0530

    Added iterator pattern

---
+++ iterator/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Iterator
+{
+  const int firstItem()
+  {
+    return data.front();
+  }
+
+  const int nextItem()
+  {
+    ++it;
+    return *it;
+  }
+
+  bool isDone()
+  {
+    return (data.end() == it);
+  }
+
+  const int currentItem()
+  {
+    return *it;
+  }
+
+protected:
+  vector<int> data;
+  vector<int>::iterator it;
+};
+
+struct ConcreteIterator : Iterator
+{
+  ConcreteIterator(int collection[])
+  {
+    data = vector<int>(collection, collection + sizeof(collection)/sizeof(int));
+    it = data.begin();
+  }
+};
+
+struct Aggregate
+{
+  virtual Iterator *createIterator() = 0;
+
+protected:
+  int data[10], idx;
+};
+
+struct ConcreteAggregate : Aggregate
+{
+  ConcreteAggregate() {
+    idx = 0;
+  }
+
+  virtual Iterator *createIterator()
+  {
+    return new ConcreteIterator(data);
+  }
+
+  void add(int n)
+  {
+    data[idx] = n;
+    ++idx;
+  }
+};
+
+int main()
+{
+  ConcreteAggregate agg;
+  agg.add(5);
+  agg.add(6);
+
+  Iterator *it = agg.createIterator();
+  cout << "ITEM: " << it->currentItem() << endl;
+  do {
+    if (it->isDone()) break;
+
+    cout << "ITEM: " << it->nextItem() << endl;
+  } while (!it->isDone());
+
+  delete it;
+
+  return 0;
+}
